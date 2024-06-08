c83e59aaa0f3a6cea7a296068c78879d2b9bc2de
TSEO team <dev@tseo.pro>
2024-06-08 13:22:12 +0200

    Added custom icons.

---
+++ sass/icons/tseoindexing_icons.dart
@@ -0,0 +1,35 @@
+// Place fonts/tseoindexing.ttf in your fonts/ directory and
+// add the following to your pubspec.yaml
+// flutter:
+//   fonts:
+//    - family: tseoindexing
+//      fonts:
+//       - asset: fonts/tseoindexing.ttf
+import 'package:flutter/widgets.dart';
+
+class Tseoindexing {
+  Tseoindexing._();
+
+  static const String _fontFamily = 'tseoindexing';
+
+  static const IconData pencil = IconData(0xe905, fontFamily: _fontFamily);
+  static const IconData film = IconData(0xe913, fontFamily: _fontFamily);
+  static const IconData paste = IconData(0xe92d, fontFamily: _fontFamily);
+  static const IconData database = IconData(0xe964, fontFamily: _fontFamily);
+  static const IconData spinner = IconData(0xe97a, fontFamily: _fontFamily);
+  static const IconData cogs = IconData(0xe995, fontFamily: _fontFamily);
+  static const IconData stats_dots = IconData(0xe99b, fontFamily: _fontFamily);
+  static const IconData fire = IconData(0xe9a9, fontFamily: _fontFamily);
+  static const IconData sphere = IconData(0xe9c9, fontFamily: _fontFamily);
+  static const IconData earth = IconData(0xe9ca, fontFamily: _fontFamily);
+  static const IconData link = IconData(0xe9cb, fontFamily: _fontFamily);
+  static const IconData eye = IconData(0xe9ce, fontFamily: _fontFamily);
+  static const IconData warning = IconData(0xea07, fontFamily: _fontFamily);
+  static const IconData info = IconData(0xea0c, fontFamily: _fontFamily);
+  static const IconData cancel_circle = IconData(0xea0d, fontFamily: _fontFamily);
+  static const IconData cross = IconData(0xea0f, fontFamily: _fontFamily);
+  static const IconData checkmark = IconData(0xea10, fontFamily: _fontFamily);
+  static const IconData checkmark2 = IconData(0xea11, fontFamily: _fontFamily);
+  static const IconData circle_right = IconData(0xea42, fontFamily: _fontFamily);
+  static const IconData circle_left = IconData(0xea44, fontFamily: _fontFamily);
+}
