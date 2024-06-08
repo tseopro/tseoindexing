fd38adcd87c39f071af85a99b8d15e28e7c8f429
TSEO team <dev@tseo.pro>
2024-06-08 21:58:11 +0200

    Updated icons

--- sass/icons/tseoindexing_icons.dart
+++ sass/icons/tseoindexing_icons.dart
@@ -16,12 +16,9 @@ class Tseoindexing {
   static const IconData film = IconData(0xe913, fontFamily: _fontFamily);
   static const IconData paste = IconData(0xe92d, fontFamily: _fontFamily);
   static const IconData database = IconData(0xe964, fontFamily: _fontFamily);
-  static const IconData spinner = IconData(0xe97a, fontFamily: _fontFamily);
   static const IconData cogs = IconData(0xe995, fontFamily: _fontFamily);
-  static const IconData stats_dots = IconData(0xe99b, fontFamily: _fontFamily);
   static const IconData fire = IconData(0xe9a9, fontFamily: _fontFamily);
   static const IconData sphere = IconData(0xe9c9, fontFamily: _fontFamily);
-  static const IconData earth = IconData(0xe9ca, fontFamily: _fontFamily);
   static const IconData link = IconData(0xe9cb, fontFamily: _fontFamily);
   static const IconData eye = IconData(0xe9ce, fontFamily: _fontFamily);
   static const IconData warning = IconData(0xea07, fontFamily: _fontFamily);
@@ -30,6 +27,5 @@ class Tseoindexing {
   static const IconData cross = IconData(0xea0f, fontFamily: _fontFamily);
   static const IconData checkmark = IconData(0xea10, fontFamily: _fontFamily);
   static const IconData checkmark2 = IconData(0xea11, fontFamily: _fontFamily);
-  static const IconData circle_right = IconData(0xea42, fontFamily: _fontFamily);
-  static const IconData circle_left = IconData(0xea44, fontFamily: _fontFamily);
+  static const IconData bin = IconData(0xe9ac, fontFamily: _fontFamily);
 }
